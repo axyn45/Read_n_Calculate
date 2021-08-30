0efea4894d0b8e5b67f64ab6d018227233ecf466
axyn45 <axyn45@outlook.com>
2021-08-30 11:26:09 +0800

    remade the whole project

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-char ctn_fun[10];
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 int main(){
-    FILE *fp=fopen("question.txt","r");
-    switch(read_line()){
-        case 0:
-            break;
-        case 1:
-            printf("The expression is in wrong format! Please recheck it in the file.\nThe program will quit...");
-            return 0;
-        case 2:
-            printf("The expression has parameter(s) in wrong format or the number of parameters exceeds limit! Please recheck it in the file.\nThe program will quit...");
-            return 0;
-    }                                  //read_line return value: 0 - success, 1 - wrong format, 2 - wrong parameters
-    
+    FILE *fp_read=fopen("question.txt","r");
+    FILE *fp_write=fopen("answer.txt","a");
+    return 0;
 }
